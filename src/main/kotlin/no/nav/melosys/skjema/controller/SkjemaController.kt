4496832c24c232b7c43a7adfdbc0e17f446930c3
Øyvind Nygard <oyvinyg@gmail.com>
2025-08-28 15:55:01 +0200

    Bruker spring-boot-starter-actuator for helse-endepunkt (#6)
    * Bruke spring-boot-actuator ut av boksen health
    * Fjernet servlet.context-path: /api og heller satt denne eksplisitt i controllerne. Dette er for å kke ha /api prefiks på actuator-endepunktene
    * Satt opp en docker compose og template for lokal spring profil for å kunne kjøre opp appen lokalt

--- src/main/kotlin/no/nav/melosys/skjema/controller/SkjemaController.kt
+++ src/main/kotlin/no/nav/melosys/skjema/controller/SkjemaController.kt
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/skjema")
+@RequestMapping("/api/skjema")
 @Tag(name = "Skjema", description = "placeholder")
 class SkjemaController {
 
