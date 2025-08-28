8a2490687fc2ff774841eba5ccd3992677407b46
oyvinyg <oyvinyg@gmail.com>
2025-08-28 15:26:30 +0200

    Bruker spring-boot-starter-actuator for helse-endepunkt

--- src/main/kotlin/no/nav/melosys/skjema/MelosysSkjemaApiApplication.kt
+++ src/main/kotlin/no/nav/melosys/skjema/MelosysSkjemaApiApplication.kt
@@ -3,6 +3,7 @@ package no.nav.melosys.skjema
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
 class MelosysSkjemaApiApplication
 
