38371c2d3b22ab69cce56f906d6af83687ba335e
oyvinyg <oyvinyg@gmail.com>
2025-08-28 19:09:09 +0200

    Satt opp testcontainers for test

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     runtimeOnly("org.postgresql:postgresql")
     
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:postgresql")
 }
 
 tasks.withType<KotlinCompile> {
