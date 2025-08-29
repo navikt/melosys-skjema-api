88f526c2554a01a5ed21302629020fef71e90435
barstadEkstern <oystein.madsen.barstad@nav.no>
2025-08-29 12:22:21 +0200

    Update auth

--- src/main/kotlin/no/nav/melosys/skjema/controller/AuthController.kt
+++ src/main/kotlin/no/nav/melosys/skjema/controller/AuthController.kt
@@ -3,20 +3,24 @@ package no.nav.melosys.skjema.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
+import no.nav.melosys.skjema.dto.RepresentasjonerRequest
+import no.nav.melosys.skjema.dto.RepresentasjonerResponse
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @Tag(name = "Authentication", description = "placeholder")
 class AuthController {
 
-    @GetMapping("/representasjoner")
+    @PostMapping("/representasjoner")
     @Operation(summary = "placeholder", description = "placeholder")
     @ApiResponse(responseCode = "200", description = "placeholder")
-    fun getRepresentasjoner(): ResponseEntity<Any> {
-        return ResponseEntity.ok().build()
+    fun getRepresentasjoner(@RequestBody request: RepresentasjonerRequest): ResponseEntity<RepresentasjonerResponse> {
+        val response = RepresentasjonerResponse(
+            fnr = emptyList(),
+            orgnr = emptyList()
+        )
+        return ResponseEntity.ok(response)
     }
 }
