a5c9efa5d5115be078fe137ee70bf757072b7db4
barstadEkstern <oystein.madsen.barstad@nav.no>
2025-08-29 12:22:40 +0200

    dtos

---
+++ src/main/kotlin/no/nav/melosys/skjema/dto/RepresentasjonerRequest.kt
@@ -0,0 +1,5 @@
+package no.nav.melosys.skjema.dto
+
+data class RepresentasjonerRequest(
+    val fnr: String
+)
---
+++ src/main/kotlin/no/nav/melosys/skjema/dto/RepresentasjonerResponse.kt
@@ -0,0 +1,6 @@
+package no.nav.melosys.skjema.dto
+
+data class RepresentasjonerResponse(
+    val fnr: List<String>,
+    val orgnr: List<String>
+)
