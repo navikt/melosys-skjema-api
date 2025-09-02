899f30e2dbba78511cf54af43367112f72812ab9
Isa Salem Hame <isa.salem.hame@nav.no>
2025-09-03 00:33:17 +0200

    Legger til Altinn API-endepunkter
    - AltinnService for forretningslogikk
    - AltinnController med endepunkter:
      - GET /api/hentTilganger for brukerens tilganger
      - GET /api/harTilgang/{orgnr} for tilgangssjekk
    - Beskyttet med @Protected annotation

---
+++ src/main/kotlin/no/nav/melosys/skjema/controller/AltinnController.kt
@@ -0,0 +1,47 @@
+package no.nav.melosys.skjema.controller
+
+import no.nav.melosys.skjema.dto.OrganisasjonDto
+import no.nav.melosys.skjema.service.AltinnService
+import no.nav.security.token.support.core.api.Protected
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api")
+@Protected
+class AltinnController(
+    private val altinnService: AltinnService
+) {
+    
+    companion object {
+        private val log = LoggerFactory.getLogger(AltinnController::class.java)
+    }
+    
+    @GetMapping("/hentTilganger")
+    suspend fun hentTilganger(): ResponseEntity<List<OrganisasjonDto>> {
+        log.info("Henter tilganger for innlogget bruker")
+        
+        val tilganger = altinnService.hentBrukersTilganger()
+        
+        return if (tilganger.isNotEmpty()) {
+            ResponseEntity.ok(tilganger)
+        } else {
+            ResponseEntity.noContent().build()
+        }
+    }
+    
+    @GetMapping("/harTilgang/{orgnr}")
+    suspend fun harTilgang(@PathVariable orgnr: String): ResponseEntity<Boolean> {
+        log.info("Sjekker tilgang til organisasjon: $orgnr")
+        
+        if (!orgnr.matches(Regex("\\d{9}"))) {
+            log.warn("Ugyldig organisasjonsnummer: $orgnr")
+            return ResponseEntity.badRequest().build()
+        }
+        
+        val harTilgang = altinnService.harBrukerTilgang(orgnr)
+        
+        return ResponseEntity.ok(harTilgang)
+    }
+}
---
+++ src/main/kotlin/no/nav/melosys/skjema/service/AltinnService.kt
@@ -0,0 +1,46 @@
+package no.nav.melosys.skjema.service
+
+import no.nav.melosys.skjema.dto.OrganisasjonDto
+import no.nav.melosys.skjema.integrasjon.altinn.ArbeidsgiverAltinnTilgangerConsumer
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class AltinnService(
+    private val arbeidsgiverAltinnTilgangerConsumer: ArbeidsgiverAltinnTilgangerConsumer
+) {
+    
+    companion object {
+        private val log = LoggerFactory.getLogger(AltinnService::class.java)
+    }
+    
+    suspend fun hentBrukersTilganger(): List<OrganisasjonDto> {
+        log.info("Henter brukers tilganger fra Altinn")
+        
+        return try {
+            val tilganger = arbeidsgiverAltinnTilgangerConsumer.hentTilganger()
+            
+            tilganger.map { org ->
+                OrganisasjonDto(
+                    orgnr = org.orgnr,
+                    navn = org.navn,
+                    organisasjonsform = org.organisasjonsform
+                )
+            }
+        } catch (e: Exception) {
+            log.error("Feil ved henting av tilganger fra Altinn", e)
+            emptyList()
+        }
+    }
+    
+    suspend fun harBrukerTilgang(orgnr: String): Boolean {
+        log.info("Sjekker om bruker har tilgang til organisasjon: $orgnr")
+        
+        return try {
+            arbeidsgiverAltinnTilgangerConsumer.harTilgang(orgnr)
+        } catch (e: Exception) {
+            log.error("Feil ved sjekk av tilgang til organisasjon $orgnr", e)
+            false
+        }
+    }
+}
