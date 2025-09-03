6c41f58a88973851dc1dcd3d23d4e19a53bd413b
Isa Salem Hame <isa.salem.hame@nav.no>
2025-09-03 02:50:45 +0200

    Fjern unødvendig bønne

--- src/main/kotlin/no/nav/melosys/skjema/config/TokenSupportConfiguration.kt
+++ src/main/kotlin/no/nav/melosys/skjema/config/TokenSupportConfiguration.kt
@@ -1,9 +1,7 @@
 package no.nav.melosys.skjema.config
 
-import no.nav.security.token.support.client.spring.oauth2.ClientConfigurationPropertiesMatcher
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @EnableOAuth2Client(cacheEnabled = true)
@@ -11,7 +9,4 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class TokenSupportConfiguration {
 
-    //TODO: Sjekk om denne er nødvendig.
-    @Bean
-    fun configMatcher() = object : ClientConfigurationPropertiesMatcher {}
 }
