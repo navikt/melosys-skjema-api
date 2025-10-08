746cc3accc8fd3f6ee50ac6d52a4c3a81da40f5a
barstadEkstern <oystein.madsen.barstad@nav.no>
2025-10-08 12:36:38 +0200

    Lag custom flyway migration

--- src/main/kotlin/no/nav/melosys/skjema/config/DatabaseConfig.kt
+++ src/main/kotlin/no/nav/melosys/skjema/config/DatabaseConfig.kt
@@ -1,6 +1,9 @@
 package no.nav.melosys.skjema.config
 
+import org.flywaydb.core.Flyway
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
@@ -9,4 +12,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement
 @EnableJpaRepositories(basePackages = ["no.nav.melosys.skjema.repository"])
 @EntityScan(basePackages = ["no.nav.melosys.skjema.entity"])
 @EnableTransactionManagement
-class DatabaseConfig
+class DatabaseConfig {
+
+    @Bean
+    fun cleanMigrateStrategy(): FlywayMigrationStrategy {
+        return FlywayMigrationStrategy { flyway: Flyway ->
+            flyway.repair()
+            flyway.migrate()
+        }
+    }
+}
