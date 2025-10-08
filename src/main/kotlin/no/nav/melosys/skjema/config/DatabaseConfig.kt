b613bb25ac0c082bfc58b098bc472242b6e72a42
barstadEkstern <oystein.madsen.barstad@nav.no>
2025-10-08 13:25:29 +0200

    fjern custom bean

--- src/main/kotlin/no/nav/melosys/skjema/config/DatabaseConfig.kt
+++ src/main/kotlin/no/nav/melosys/skjema/config/DatabaseConfig.kt
@@ -1,9 +1,6 @@
 package no.nav.melosys.skjema.config
 
-import org.flywaydb.core.Flyway
 import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
@@ -12,13 +9,4 @@ import org.springframework.transaction.annotation.EnableTransactionManagement
 @EnableJpaRepositories(basePackages = ["no.nav.melosys.skjema.repository"])
 @EntityScan(basePackages = ["no.nav.melosys.skjema.entity"])
 @EnableTransactionManagement
-class DatabaseConfig {
-
-    @Bean
-    fun cleanMigrateStrategy(): FlywayMigrationStrategy {
-        return FlywayMigrationStrategy { flyway: Flyway ->
-            flyway.repair()
-            flyway.migrate()
-        }
-    }
-}
+class DatabaseConfig
