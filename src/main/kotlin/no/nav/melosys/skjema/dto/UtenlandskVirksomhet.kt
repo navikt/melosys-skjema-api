ce8da8ba673fbdafc0a52ecf58266df8e73cd003
oyvinyg <oyvinyg@gmail.com>
2025-10-10 11:59:16 +0200

    Gjøre valgfrie felter nullable i UtenlandskVirksomhet

--- src/main/kotlin/no/nav/melosys/skjema/dto/UtenlandskVirksomhet.kt
+++ src/main/kotlin/no/nav/melosys/skjema/dto/UtenlandskVirksomhet.kt
@@ -2,12 +2,12 @@ package no.nav.melosys.skjema.dto
 
 data class UtenlandskVirksomhet(
     val navn: String,
-    val organisasjonsnummer: String,
+    val organisasjonsnummer: String?,
     val vegnavnOgHusnummer: String,
     val bygning: String?,
-    val postkode: String,
-    val byStedsnavn: String,
-    val region: String,
+    val postkode: String?,
+    val byStedsnavn: String?,
+    val region: String?,
     val land: String,
     val tilhorerSammeKonsern: Boolean
 )
