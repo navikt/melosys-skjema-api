e512a1c7fb659c499196b1871045a5021371563f
Isa Salem Hame <isa.salem.hame@nav.no>
2025-11-19 00:05:56 +0100

    Oppdater cache config og dependencies

--- src/main/kotlin/no/nav/melosys/skjema/config/CacheConfig.kt
+++ src/main/kotlin/no/nav/melosys/skjema/config/CacheConfig.kt
@@ -1,8 +1,36 @@
 package no.nav.melosys.skjema.config
 
+import com.github.benmanes.caffeine.cache.Caffeine
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.caffeine.CaffeineCacheManager
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.util.concurrent.TimeUnit
 
 @Configuration
 @EnableCaching
-class CacheConfig
+class CacheConfig {
+
+    @Bean
+    fun cacheManager(
+        @Value("\${repr.cache.ttl-minutes:5}") reprCacheTtlMinutes: Long
+    ): CacheManager {
+        val cacheManager = CaffeineCacheManager()
+
+        // Registrer alle cache-navn
+        cacheManager.setCacheNames(listOf("fullmakter"))
+
+        // Konfigurasjon per cache
+        cacheManager.registerCustomCache(
+            "fullmakter",
+            Caffeine.newBuilder()
+                .expireAfterWrite(reprCacheTtlMinutes, TimeUnit.MINUTES)
+                .maximumSize(1000)
+                .build()
+        )
+
+        return cacheManager
+    }
+}
