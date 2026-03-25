2983190c1b8b4bbdb6b17639a04b947e396fb38b
oyvinyg <oyvinyg@gmail.com>
2026-03-25 13:26:00 +0100

    Legg til opprettetDato og endretDato i SkjemaDto
    Eksponerer opprettetDato og endretDato som LocalDateTime i
    SkjemaDto-interfacet og UtsendtArbeidstakerSkjemaDto. Konverterer
    fra Instant til LocalDateTime med Europe/Oslo tidssone i mappingen.
    
    Frontend trenger datoene for å vise når skjemaet ble opprettet og
    sist endret.

--- melosys-skjema-api-types/src/main/kotlin/no/nav/melosys/skjema/types/SkjemaDto.kt
+++ melosys-skjema-api-types/src/main/kotlin/no/nav/melosys/skjema/types/SkjemaDto.kt
@@ -2,6 +2,7 @@ package no.nav.melosys.skjema.types
 
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import java.time.LocalDateTime
 import java.util.UUID
 import no.nav.melosys.skjema.types.common.SkjemaStatus
 import no.nav.melosys.skjema.types.utsendtarbeidstaker.UtsendtArbeidstakerSkjemaDto
@@ -16,6 +17,8 @@ interface SkjemaDto {
     val type: SkjemaType
     val fnr: String
     val orgnr: String
+    val opprettetDato: LocalDateTime
+    val endretDato: LocalDateTime
     val metadata: SkjemaMetadata
     val data: SkjemaData
 }
--- melosys-skjema-api-types/src/main/kotlin/no/nav/melosys/skjema/types/utsendtarbeidstaker/UtsendtArbeidstakerSkjemaDto.kt
+++ melosys-skjema-api-types/src/main/kotlin/no/nav/melosys/skjema/types/utsendtarbeidstaker/UtsendtArbeidstakerSkjemaDto.kt
@@ -1,5 +1,6 @@
 package no.nav.melosys.skjema.types.utsendtarbeidstaker
 
+import java.time.LocalDateTime
 import java.util.UUID
 import no.nav.melosys.skjema.types.SkjemaDto
 import no.nav.melosys.skjema.types.SkjemaType
@@ -11,6 +12,8 @@ data class UtsendtArbeidstakerSkjemaDto(
     override val type: SkjemaType = SkjemaType.UTSENDT_ARBEIDSTAKER,
     override val fnr: String,
     override val orgnr: String,
+    override val opprettetDato: LocalDateTime,
+    override val endretDato: LocalDateTime,
     override val metadata: UtsendtArbeidstakerMetadata,
     override val data: UtsendtArbeidstakerSkjemaData
 ) : SkjemaDto
