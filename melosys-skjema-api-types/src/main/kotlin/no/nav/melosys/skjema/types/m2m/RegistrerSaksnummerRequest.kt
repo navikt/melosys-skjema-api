ca88aee4be6ca565a4af0c9e8a3ccf26f9cdccc8
Isa Salem Hame <isa.salem.hame@nav.no>
2026-04-08 18:51:03 +0200

    MELOSYS-7999 M2M-endepunkt for registrering av saksnummer

---
+++ melosys-skjema-api-types/src/main/kotlin/no/nav/melosys/skjema/types/m2m/RegistrerSaksnummerRequest.kt
@@ -0,0 +1,5 @@
+package no.nav.melosys.skjema.types.m2m
+
+data class RegistrerSaksnummerRequest(
+    val saksnummer: String
+)
---
+++ src/main/resources/db/migration/V16__Add_saksnummer_to_innsending.sql
@@ -0,0 +1,1 @@
+ALTER TABLE innsending ADD COLUMN saksnummer VARCHAR(99);
