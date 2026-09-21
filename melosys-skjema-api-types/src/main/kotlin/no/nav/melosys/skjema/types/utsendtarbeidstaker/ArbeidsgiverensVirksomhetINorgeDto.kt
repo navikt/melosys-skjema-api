dfcd5bed27f4d0603f664dd5132919eb9303a392
Odin Eilertsen <odin.eilertsen@nav.no>
2026-09-21 11:39:01 +0200

    MELOSYS-7670 Marker historisk V1-felt som deprecated

--- melosys-skjema-api-types/src/main/kotlin/no/nav/melosys/skjema/types/utsendtarbeidstaker/ArbeidsgiverensVirksomhetINorgeDto.kt
+++ melosys-skjema-api-types/src/main/kotlin/no/nav/melosys/skjema/types/utsendtarbeidstaker/ArbeidsgiverensVirksomhetINorgeDto.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class ArbeidsgiverensVirksomhetINorgeDto(
+    @Deprecated("Brukes kun til visning av historiske V1 innsendinger, kan fjernes når visning av disse ikke er nødvendig lenger")
     val erArbeidsgiverenOffentligVirksomhet: Boolean? = null,
     val erArbeidsgiverenBemanningsEllerVikarbyraa: Boolean? = null,
     val opprettholderArbeidsgiverenVanligDrift: Boolean? = null
